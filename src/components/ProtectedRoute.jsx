fa02f76646abc5db1e88d63578b3f05e19794185
ashutoshaditya05 <ashutoshaditya462@gmail.com>
2026-07-18 18:01:51 +0530

    admon ticket

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -4,4 +4,4 @@ export default function ProtectedRoute({ children }) {
   const token = localStorage.getItem("accessToken");
   if (!token) return <Navigate to="/login" replace />;
   return children;
-}
+} 
